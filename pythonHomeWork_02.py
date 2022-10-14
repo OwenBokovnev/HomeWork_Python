bff2c971a434a790a66c1a208935818401776d59
OwenBokovnev <ebokovnev@gmail.com>
2022-10-14 09:21:50 +0300

    Homework02 added

--- pythonHomeWork_02.py
+++ pythonHomeWork_02.py
@@ -74,25 +74,5 @@
 # Но теперь количество предикатов не три, а генерируется случайным образом от 5 до 25, проверяем это утверждение 100 раз, 
 # с помощью модуля time выводим на экран , сколько времени отработала программа.
 
-def inputNumbers(x):
-    xyz = ["X", "Y", "Z"]
-    a = []
-    for i in range(x):
-        a.append(input(f"Введите значение {xyz[i]}: "))
-    return a
 
-
-def checkPredicate(x):
-    left = not (x[0] or x[1] or x[2])
-    right = not x[0] and not x[1] and not x[2]
-    result = left == right
-    return result
-
-
-statement = inputNumbers(3)
-
-if checkPredicate(statement) == True:
-    print(f"Утверждение истинно")
-else:
-    print(f"Утверждение ложно")
 
