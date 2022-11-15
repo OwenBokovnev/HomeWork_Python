d356b5ee383f5f776328106fc9e74a3b0787b1c2
OwenBokovnev <ebokovnev@gmail.com>
2022-11-15 11:29:38 +0300

    Task 02 added

--- pythonHomeWork_06.py
+++ pythonHomeWork_06.py
@@ -1,60 +1,85 @@
 #Задача 1. Создайте программу для игры в "Крестики-нолики".
 
-print("*" * 10, " Игра Крестики-нолики для двух игроков ", "*" * 10)
+# print("*" * 10, " Игра Крестики-нолики для двух игроков ", "*" * 10)
 
-board = list(range(1,10))
+# board = list(range(1,10))
 
-def draw_board(board):
-   print("-" * 13)
-   for i in range(3):
-      print("|", board[0+i*3], "|", board[1+i*3], "|", board[2+i*3], "|")
-      print("-" * 13)
+# def draw_board(board):
+#    print("-" * 13)
+#    for i in range(3):
+#       print("|", board[0+i*3], "|", board[1+i*3], "|", board[2+i*3], "|")
+#       print("-" * 13)
 
-def take_input(player_token):
-   valid = False
-   while not valid:
-      player_answer = input("Куда поставим " + player_token+"? ")
-      try:
-         player_answer = int(player_answer)
-      except:
-         print("Некорректный ввод. Проверьте введенный символ!")
-         continue
-      if player_answer >= 1 and player_answer <= 9:
-         if(str(board[player_answer-1]) not in "XO"):
-            board[player_answer-1] = player_token
-            valid = True
-         else:
-            print("Эта клетка уже занята!")
-      else:
-        print("Некорректный ввод. Введите число от 1 до 9.")
+# def take_input(player_token):
+#    valid = False
+#    while not valid:
+#       player_answer = input("Куда поставим " + player_token+"? ")
+#       try:
+#          player_answer = int(player_answer)
+#       except:
+#          print("Некорректный ввод. Проверьте введенный символ!")
+#          continue
+#       if player_answer >= 1 and player_answer <= 9:
+#          if(str(board[player_answer-1]) not in "XO"):
+#             board[player_answer-1] = player_token
+#             valid = True
+#          else:
+#             print("Эта клетка уже занята!")
+#       else:
+#         print("Некорректный ввод. Введите число от 1 до 9.")
 
-def check_win(board):
-   win_coord = ((0,1,2), (3,4,5), (6,7,8), (0,3,6), (1,4,7), (2,5,8), (0,4,8), (2,4,6))
-   for each in win_coord:
-       if board[each[0]] == board[each[1]] == board[each[2]]:
-          return board[each[0]]
-   return False
+# def check_win(board):
+#    win_coord = ((0,1,2), (3,4,5), (6,7,8), (0,3,6), (1,4,7), (2,5,8), (0,4,8), (2,4,6))
+#    for each in win_coord:
+#        if board[each[0]] == board[each[1]] == board[each[2]]:
+#           return board[each[0]]
+#    return False
 
-def main(board):
-    counter = 0
-    win = False
-    while not win:
-        draw_board(board)
-        if counter % 2 == 0:
-           take_input("X")
-        else:
-           take_input("O")
-        counter += 1
-        if counter > 4:
-           winner = check_win(board)
-           if winner:
-              print(winner, "выиграл!")
-              win = True
-              break
-        if counter == 9:
-            print("Ничья!")
-            break
-    draw_board(board)
-main(board)
+# def main(board):
+#     counter = 0
+#     win = False
+#     while not win:
+#         draw_board(board)
+#         if counter % 2 == 0:
+#            take_input("X")
+#         else:
+#            take_input("O")
+#         counter += 1
+#         if counter > 4:
+#            winner = check_win(board)
+#            if winner:
+#               print(winner, "выиграл!")
+#               win = True
+#               break
+#         if counter == 9:
+#             print("Ничья!")
+#             break
+#     draw_board(board)
+# main(board)
 
-input("Нажмите Enter для выхода!")
+# input("Нажмите Enter для выхода!")
+
+# Задача 2. Напишите программу вычисления арифметического выражения заданного строкой. Используйте операции +,-,/,*. 
+# приоритет операций стандартный.
+# *Пример:* 
+# 2+2 => 4; 
+# 1+2*3 => 7; 
+# 1-2*3 => -5;
+# - Добавьте возможность использования скобок, меняющих приоритет операций.
+# *Пример:* 
+# 1+2*3 => 7; 
+# (1+2)*3 => 9;
+
+
+spicok = input('Введите выражение в виде (x=2+2): ')
+x = 0
+new = exec(spicok)
+print('Результат вычисления выражения: ', x)
+
+
+
+
+
+
+
+
